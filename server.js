3f1a07d23935286f6d640ba2ddd6d4d8224c1b97
Kartik Tara <91429848+KartikTara1234@users.noreply.github.com>
2025-09-01 12:53:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://frontend-nu-seven-98.vercel.app/",
+    origin: "http://hospital-ochre-five.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
